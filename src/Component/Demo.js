79aef83283891efca6e11e85cd42b8c4ea73fd83
mitalitagadiya <6320mitalitagadiya@gmail.com>
2022-05-27 03:03:52 -0700

    27_5

---
+++ src/Component/Demo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Demo extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Mitali</h2>
+            </div>
+        );
+    }
+}
+
+export default Demo;

6debcf50a20d44b5a9b308c88f5323faaa0d5f88
mitalitagadiya <106071619+mitalitagadiya@users.noreply.github.com>
2022-06-03 13:15:34 +0530

    30_5(4)

--- src/App.js
+++ src/App.js
@@ -5,30 +5,25 @@ import Demo from './Component/Demo';
 function App() {
 
 
-  const person = [
-    {
-      name: "Amit",
-      age: 25,
-      course: [
-        "C",
-        "HTML"
-      ]
-    },
-    {
-      name: "Ajay",
-      age: 40,
-      course: [
-        "Java",
-        "JavaScript"
-      ]
+  const myObj = {
+    name: "John",
+    age: 30,
+    cars: {
+      car1: "Ford",
+      car2: "BMW",
+      car3: "Fiat"
     }
-  ]
+  }
 
-  person.map((p) => {
-    console.log(person.name, person.age);
-    person.course.map((p) => console.log(p));
-  })
-
+  for (let k in myObj) {
+    if (k === "cars") {
+      for (let j in myObj[k]) {
+        console.log(myObj[k][j]);
+      }
+    } else {
+      console.log(myObj[k]);
+    }
+  }
 
   
 

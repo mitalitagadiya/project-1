1030d46d2bf02f063bc5ce36e7ad92f804f1718a
mitalitagadiya <6320mitalitagadiya@gmail.com>
2022-06-09 16:00:41 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,6 @@
-import React, { Component, useEffect, useState } from 'react';
-import Demo from './Component/Demo';
-import Home from './Containers/Home';
-import Loading from './Containers/Loading';
 
 
-const HomewithLoading = Loading(Home);
-
-function App(props) {
-
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState ([ ]);
-
-  const orgData = [
-    {id:101, name:"mitali"},
-    {id:102, name:"ruchika"}
-  ]
-
- useEffect(() => {
-   setLoading(true);
-   setTimeout(() =>{setLoading(false); setData(orgData)}, 2000)
- }
- ,[])
-
- return(
-   <HomewithLoading
-      isLoading ={loading}
-      data ={data}
-   />
- )
-
-}
 
 
-export default App;
+
+
